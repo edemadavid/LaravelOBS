e86a468ccbe1ce36aa4d66c10c446c1d3179176a
DAVID EDEMA <edema.davidovie@gmail.com>
2022-08-10 17:05:46 +0100

    role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,12 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username');
+            $table->string('phone_number');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->integer('role_id')->reference('id')->on('role');
             $table->timestamps();
         });
     }
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        // \DB::table('roles')->delete();
+
+        // \DB::table('roles')->insert(array (
+        //     0 =>
+        //     array (
+        //         'id' => 1,
+        //         'name' => 'SuperAdmin',
+        //         'created_at' => '2022-03-30 06:23:00',
+        //         'updated_at' => '2022-03-30 06:23:00',
+        //     ),
+        //     1 =>
+        //     array (
+        //         'id' => 2,
+        //         'name' => 'Accountant',
+        //         'created_at' => '2022-03-30 05:45:00',
+        //         'updated_at' => '2022-03-30 05:45:00',
+        //     ),
+        //     2 =>
+        //     array (
+        //         'id' => 3,
+        //         'name' => 'Customer',
+        //         'created_at' => '2022-08-03 10:16:10',
+        //         'updated_at' => '2022-08-03 10:16:10',
+        //     ),
+        //     3 =>
+        //     array (
+        //         'id' => 4,
+        //         'name' => 'User',
+        //         'created_at' => '2022-08-03 10:16:10',
+        //         'updated_at' => '2022-08-03 10:16:10',
+        //     ),
+        // ));
+    }
+}
